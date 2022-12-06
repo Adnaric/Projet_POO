59123c74bc69c97c5c5f470b11d9e895c952849f
Acegenesis <66308616+Acegenesis@users.noreply.github.com>
2022-12-06 09:55:32 +0100

    Nickel pas d'erreurs

--- POO/CLservices.h
+++ POO/CLservices.h
@@ -8,8 +8,19 @@ namespace NS_Comp_Svc
 	{
 	private:
 		NS_Comp_Data::CLcad^ oCad;
-		NS_Comp_Mappage::CLmapTB^ oMappTB;
+		NS_Comp_Mappage::CLmapPersonnel^ oMappPersonnel;
+		NS_Comp_Mappage::CLmapClient^ oMappClient;
+		NS_Comp_Mappage::CLmapArticle^ oMappArticle;
+		NS_Comp_Mappage::CLmapCommande^ oMappCommande;
 	public:
 		CLservices(void);
+		System::Data::DataSet^ selection_tout_personnel(System::String^);
+		System::Data::DataSet^ selection_tout_client(System::String^);
+		System::Data::DataSet^ selection_tout_article(System::String^);
+		System::Data::DataSet^ selection_tout_commande(System::String^);
+		void ajouter_personnel(System::String^, System::String^, int, System::String^, int, System::String^, System::String^, int);
+		void ajouter_client(System::String^, System::String^, System::String^, int, System::String^, System::String^, int, int, System::String^, System::String^, int);
+		void ajouter_article(float, System::String^, System::String^, int);
+		void ajouter_commande(int, System::String^, System::String^, System::String^, int, System::String^, int);
 	};
 }
